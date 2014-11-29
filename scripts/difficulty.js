49944f756670ccfbf62809321fe447d09daed274
egles111 <alex.eagles2@gmail.com>
2014-11-30 01:00:37 +1100

    Round number

--- scripts/difficulty.js
+++ scripts/difficulty.js
@@ -1,8 +1,8 @@
-var difficulty = { roundNumber: 0, houseNumber: 2, treeNumber: 200, fireManCost: 600, cutTreeCost: 400, waterBombCost: 1000 };
-var player = { funds: 10000 };
+var difficulty = { houseNumber: 2, treeNumber: 200, fireManCost: 600, cutTreeCost: 400, waterBombCost: 1000 };
+var player = { funds: 10000, roundNumber: 0 };
 
 function increaseDifficulty() {
-  difficulty.roundNumber++;
+  player.roundNumber++;
   difficulty.houseNumber++;
   difficulty.treeNumber += 50;
   difficulty.fireManCost += 25;
